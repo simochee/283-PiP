9dc56c307ebccbd0aef85dd63d19367ad0dae660
simochee <lollipop@simochee.net>
2024-12-11 00:47:10 +0900

    fix: manifest version

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "wxt";
+import pkg from "./package.json" assert { type: "json" };
 import { COMMAND_START_PIP } from "./utils/const";
 
 export default defineConfig({
 	manifest: {
 		name: "283 PiP",
-		version: "1.0.0",
+		version: pkg.version,
 		description:
 			"enza 版 アイドルマスター シャイニーカラーズ のゲームプレイ画面を小窓 (Picture-in-Picture) で表示します。",
 		permissions: ["action", "contextMenus", "scripting"],
