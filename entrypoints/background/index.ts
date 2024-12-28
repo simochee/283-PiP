89795eb3385cf30d4ceaa1eab237f6a2b897a83a
simochee <lollipop@simochee.net>
2024-12-28 15:16:55 +0900

    feat: quick links

--- entrypoints/background/index.ts
+++ entrypoints/background/index.ts
@@ -2,11 +2,13 @@ import { createAlbumLinks } from "./albumLinks";
 import { handleClickIcon } from "./browserAction";
 import { handleCommand } from "./command";
 import { createContextMenu } from "./contextMenu";
+import { createQuickLinks } from "./quickLinks";
 
 export default defineBackground({
 	type: "module",
 	main() {
 		createContextMenu();
+		createQuickLinks();
 		createAlbumLinks();
 		handleCommand();
 		handleClickIcon();
